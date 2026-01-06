fd52ec3a97460c947ca5dc1d6635d4fcbc83c58f
Ivanna Parfeniuk <parfeniuk.ivannaa@gmail.com>
2026-01-06 20:56:12 +0200

    Add FormAdmin

--- job_application/admin.py
+++ job_application/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Form
 
-admin.site.register(Form)
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "email", "occupation")
+    list_filter = ("date", "occupation")
+    ordering = ("-date",)
+    readonly_fields = ("email", "occupation")
+
+
+admin.site.register(Form, FormAdmin)
